26dc49ce0cfe4dd76ea9674b511845828a85dbfd
mtadamek <michal@adamek.hub.pl>
2017-10-12 13:01:51 +0200

    0.1
    commit 1

--- Ukrywacz/Form1.h
+++ Ukrywacz/Form1.h
@@ -50,7 +50,7 @@ namespace Ukrywacz {
 		{
 			this->SuspendLayout();
 			// 
-			// Form1132132
+			// TEST
 			// 
 			this->AutoScaleDimensions = System::Drawing::SizeF(6, 13);
 			this->AutoScaleMode = System::Windows::Forms::AutoScaleMode::Font;
